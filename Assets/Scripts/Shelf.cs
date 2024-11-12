05a753a61c9b0159d34281607a34ff36acb34d65
YUJUN-LEE <154584046+YUJUN-LEE@users.noreply.github.com>
2024-11-12 21:00:36 +0900

    Map Update and Shelf Code Update

--- Assets/Scripts/Shelf.cs
+++ Assets/Scripts/Shelf.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class Shelf : Interactable 
 {
-
-
+    
+    public Vector3 Place1;
+    public Vector3 Place2;
+    public Vector3 Place3;
+    public Vector3 Place4;
+    public Vector3 Place5;
+    public Vector3 Place6;
     public override void Interact() {
         Debug.Log($"{interactableName} is interacted");
+    }
+    public void ShelfUpdate()
+    {
+        
     }
 }
